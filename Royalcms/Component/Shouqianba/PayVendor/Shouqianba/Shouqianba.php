0f0b6927fbfa2cd35ee872020b0f606024739bbf
王正东 <wangzhengdong@ecmoban.com>
2020-12-09 18:16:05 +0800

    修复收银台自定义网关路径

--- Royalcms/Component/Shouqianba/PayVendor/Shouqianba/Shouqianba.php
+++ Royalcms/Component/Shouqianba/PayVendor/Shouqianba/Shouqianba.php
@@ -108,7 +108,7 @@ class Shouqianba implements GatewayApplicationInterface
     {
         $this->payload = $order;
 
-        $gateway = get_class($this).'\\'.Str::studly('refund').'Gateway';
+        $gateway = get_class($this).'\\Gateways\\'.Str::studly('refund').'Gateway';
 
         if (class_exists($gateway)) {
             return $this->makePay($gateway);
